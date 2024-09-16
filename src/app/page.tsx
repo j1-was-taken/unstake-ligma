46ff3f01ebb52db969d48b37ca6deddf2e88157c
jake <jacobroach99@gmail.com>
2024-09-16 13:19:47 -0700

    staking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,35 @@
 
 import React, { useState, useEffect } from 'react';
 import { PublicKey } from '@solana/web3.js';
-import { connectWallet, unstakeLigmaTokens } from '../util/unstake';
+import { connectWallet, unstakeLigmaTokens, stakeLigmaTokens, getAccountBalance } from '../util/unstake';
 
 const Home: React.FC = () => {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
   const [amount, setAmount] = useState('')
-  const [text, setText] = useState('')
+  const [textStake, setTextStake] = useState('')
+  const [maxLigma, setMaxLigma] = useState(0)
+  const [maxXLigma, setMaxXLigma] = useState(0)
+  const [isLoadingStake, setIsLoadingStake] = useState(false);
+
+  const LIGMA_ADDRESS = new PublicKey("node3SHFNF7h6N9jbztfVcXrZcvAJdns1xAV8CbYFLG");
+  const XLIGMA_ADDRESS = new PublicKey("xNodeyB1u8WNrKQJqfucbKDMq7LYcAQfYXmqVdDj9M5");
 
   useEffect(() => {
     const checkIfWalletIsConnected = async () => {
-      if ('solana' in window) {
-        const { solana } = window as any;
-        if (solana.isPhantom) {
+      if ('solflare' in window) {
+        const solana = window as any;
+        if (solana.solflare.isConnected) {
           try {
-            const response = await solana.connect({ onlyIfTrusted: true });
+            const response = await solana.solflare.connect({ onlyIfTrusted: true });
             console.log('Wallet already connected')
             setWalletAddress(response.publicKey.toString());
-
           } catch (error) {
             console.error(error)
           }
         }
       }
     };
+
     checkIfWalletIsConnected();
   }, []);
 
@@ -32,19 +38,69 @@ const Home: React.FC = () => {
     const address = await connectWallet();
     if (address) {
       setWalletAddress(address);
+      setMaxBalances();
     }
   };
+
+  const setMaxBalances = async () => {
+    if ('solflare' in window) {
+      const solana = window as any;
+      const ligmaBalance = await getAccountBalance(new PublicKey(solana.solflare.publicKey), LIGMA_ADDRESS);
+      setMaxLigma(ligmaBalance);
+      const xligmaBalance = await getAccountBalance(new PublicKey(solana.solflare.publicKey), XLIGMA_ADDRESS);
+      setMaxXLigma(xligmaBalance);
+    }
+  }
 
   const handleUnstakeLigma = async () => {
     if (walletAddress) {
       try {
-        const signature = await unstakeLigmaTokens(new PublicKey(walletAddress), parseFloat(amount));
-        setText('https://solscan.io/tx/' + signature);
+        const { signature, confirmation } = await unstakeLigmaTokens(new PublicKey(walletAddress), parseFloat(amount));
+        console.log(signature)
+        setTextStake('https://solscan.io/tx/' + signature + '<br /><br />Confirming Transaction...');
+        const confirmationStatus = await confirmation;
+        setTextStake('https://solscan.io/tx/' + signature + '<br /><br />' + confirmationStatus);
+        await setMaxBalances();
       } catch (err) {
         console.error('Transaction failed!', err);
       }
     }
   };
+
+  const handleStakeLigma = async () => {
+    if (walletAddress) {
+      try {
+        const { signature, confirmation } = await stakeLigmaTokens(new PublicKey(walletAddress), parseFloat(amount));
+        setTextStake('https://solscan.io/tx/' + signature + '<br /><br />Confirming Transaction...');
+        const confirmationStatus = await confirmation;
+        setTextStake('https://solscan.io/tx/' + signature + '<br /><br />' + confirmationStatus);
+        await setMaxBalances();
+      } catch (err) {
+        console.error('Transaction failed!', err);
+      }
+    }
+  };
+
+  const handleSetMaxUnstake = async () => {
+    if (walletAddress) {
+      try {
+        setAmount(String(maxXLigma)); // Update the amount state variable
+      } catch (err) {
+        console.error('Transaction failed!', err);
+      }
+    }
+  };
+
+  const handleSetMaxStake = async () => {
+    if (walletAddress) {
+      try {
+        setAmount(String(maxLigma)); // Update the amount state variable
+      } catch (err) {
+        console.error('Transaction failed!', err);
+      }
+    }
+  };
+
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '100px' }}>
@@ -52,11 +108,20 @@ const Home: React.FC = () => {
       {!walletAddress ? (
         <button onClick={handleConnectWallet}>Connect Wallet</button>
       ) : (
-        <div style={{display: 'flex', flexDirection: 'column', padding: 20, gap: 5, maxWidth: 300}}>
-          <label htmlFor="input">xligma token amount</label>
-          <input type="text" placeholder='50000' value={amount} onChange={e => setAmount(e.target.value)}/>
-          <button onClick={handleUnstakeLigma}>Unstake Ligma</button>
-          <p style={{wordWrap: 'break-word'}}>{text}</p>
+        <div style={{ display: 'flex', flexDirection: 'column', padding: 20, gap: 5, maxWidth: 300 }}>
+          <label htmlFor="input" style={{marginLeft: "37px"}}>xligma token amount</label>
+          <input type="text" value={amount} onChange={e => setAmount(e.target.value)} />
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <button onClick={handleUnstakeLigma} style={{ width: '100px', height: '20px' }}>Unstake Ligma</button>
+            <a href="#" onClick={handleSetMaxUnstake} style={{ marginLeft: '10px', color: 'blue', textDecoration: 'underline' }}>Max:{maxXLigma}</a>
+          </div>
+
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <button onClick={handleStakeLigma} style={{ width: '100px', height: '20px' }}>Stake Ligma</button>
+            <a href="#" onClick={handleSetMaxStake} style={{ marginLeft: '10px', color: 'blue', textDecoration: 'underline' }}>Max:{String(maxLigma)}</a>
+          </div>
+
+          <p style={{ wordWrap: 'break-word' }} dangerouslySetInnerHTML={{ __html: textStake }}></p>
         </div>
       )}
     </div>
--- src/util/unstake.ts
+++ src/util/unstake.ts
@@ -5,8 +5,12 @@ import {
   VersionedTransaction,
   ComputeBudgetProgram,
   TransactionMessage,
+  Transaction,
+  sendAndConfirmTransaction,
+  LAMPORTS_PER_SOL,
 } from '@solana/web3.js';
-import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { createAssociatedTokenAccountInstruction, getAccount, getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import bs58 from 'bs58'
 
 // Constants
 const RPC_URL = 'https://solana-mainnet.core.chainstack.com/c66e6078957a0376aae2632af596dd66/';
@@ -16,19 +20,24 @@ const XLIGMA_ADDRESS = new PublicKey("xNodeyB1u8WNrKQJqfucbKDMq7LYcAQfYXmqVdDj9M
 const CONSTANT_ACCOUNT = new PublicKey("ENz6c4ZVYedrcK5V4fh7vwDA1SvZDNDQb1j3KKQbbo8Q");
 
 export const connectWallet = async (): Promise<string | null> => {
-  if ('solana' in window) {
-    const { solana } = window as any;
-    if (solana.isPhantom) {
+  if ('solflare' in window) {
+    const solana = window as any;
+    if (solana.solflare.isSolflare) {
       try {
-        const response = await solana.connect();
-        console.log('Connected to wallet')
-        return response.publicKey.toString();
+        const response = await solana.solflare.connect();
+        if (solana.solflare.isConnected) {
+          console.log('Connected to wallet');
+        } else {
+          console.log('Cancelled');
+        }
+
+        return solana.solflare.publicKey;
       } catch (err) {
         console.error('Connection to wallet failed!', err);
         return null;
       }
     } else {
-      alert('Phantom wallet not found! Please install it.');
+      alert('Phantom or Solflare wallet not found! Please install it.');
       return null;
     }
   }
@@ -37,9 +46,32 @@ export const connectWallet = async (): Promise<string | null> => {
 
 export const unstakeLigmaTokens = async (payer: PublicKey, amount: number) => {
   const connection = new Connection(RPC_URL, 'finalized');
+  const solana = window as any;
 
   const xligmaTokenAccount = await getAssociatedTokenAddress(XLIGMA_ADDRESS, payer);
   const ligmaTokenAccount = await getAssociatedTokenAddress(LIGMA_ADDRESS, payer);
+
+  // if (ligmaTokenAccount === null) {
+  //   const createLigmaAccountInstructions = new Transaction()
+  //     .add(
+  //       ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1_000_000 }),
+  //       ComputeBudgetProgram.setComputeUnitLimit({ units: 100_000 }),
+  //       createAssociatedTokenAccountInstruction(
+  //         payer,
+  //         ligmaTokenAccount,
+  //         payer,
+  //         LIGMA_ADDRESS
+  //       )
+  //     );
+
+  //   console.log('Creating Ligma Account...')
+
+  //   // Send the transaction
+  //   const signedTransaction = await solana.solflare.signTransaction(createLigmaAccountInstructions);
+  //   const signature = await connection.sendTransaction(signedTransaction);
+
+  //   console.log(`...Ligma Account Created at tx: ${signature}`)
+  // }
 
   const dataFunctionName = '5a5f6b2acd7c32e1fe';
   const splAmount = Buffer.alloc(8);
@@ -60,7 +92,7 @@ export const unstakeLigmaTokens = async (payer: PublicKey, amount: number) => {
   });
 
   const computeBudget = ComputeBudgetProgram.setComputeUnitLimit({ units: 100_000 });
-  const computeBudgetPriority = ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1_000_000 });
+  const computeBudgetPriority = ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1_333_333 });
 
   const { blockhash } = await connection.getLatestBlockhash('finalized');
 
@@ -72,9 +104,80 @@ export const unstakeLigmaTokens = async (payer: PublicKey, amount: number) => {
 
   const transaction = new VersionedTransaction(message);
 
-  const { solana } = window as any;
-  const signedTransaction = await solana.signTransaction(transaction);
+  const signedTransaction = await solana.solflare.signTransaction(transaction);
   const signature = await connection.sendTransaction(signedTransaction);
 
-  return signature;
+  const latestBlockHash = await connection.getLatestBlockhash();
+  const confirmationPromise = connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: signature,
+  }).then(() => 'Confirmed!');
+
+  return { signature, confirmation: confirmationPromise };
 };
+
+export const stakeLigmaTokens = async (payer: PublicKey, amount: number) => {
+  const connection = new Connection(RPC_URL, 'finalized');
+
+  const xligmaTokenAccount = await getAssociatedTokenAddress(XLIGMA_ADDRESS, payer);
+  const ligmaTokenAccount = await getAssociatedTokenAddress(LIGMA_ADDRESS, payer);
+
+  const dataFunctionName = 'ceb0ca12c8d1b36cfe';
+  const splAmount = Buffer.alloc(8);
+  splAmount.writeBigUInt64LE(BigInt(amount * 1_000_000)); // Example amount
+
+  const buyInstruction = new TransactionInstruction({
+    programId: LIGMA_PROGRAM_ID,
+    keys: [
+      { pubkey: LIGMA_ADDRESS, isSigner: false, isWritable: false },
+      { pubkey: XLIGMA_ADDRESS, isSigner: false, isWritable: true },
+      { pubkey: ligmaTokenAccount, isSigner: false, isWritable: true },
+      { pubkey: payer, isSigner: true, isWritable: true },
+      { pubkey: CONSTANT_ACCOUNT, isSigner: false, isWritable: true },
+      { pubkey: xligmaTokenAccount, isSigner: false, isWritable: true },
+      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+    ],
+    data: Buffer.from(dataFunctionName + splAmount.toString('hex'), 'hex'),
+  });
+
+  const computeBudget = ComputeBudgetProgram.setComputeUnitLimit({ units: 100_000 });
+  const computeBudgetPriority = ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1_333_333 });
+
+  const { blockhash } = await connection.getLatestBlockhash('finalized');
+
+  const message = new TransactionMessage({
+    payerKey: payer,
+    recentBlockhash: blockhash,
+    instructions: [computeBudget, computeBudgetPriority, buyInstruction],
+  }).compileToV0Message();
+
+  const transaction = new VersionedTransaction(message);
+
+  const solana = window as any;
+  const signedTransaction = await solana.solflare.signTransaction(transaction);
+  const signature = await connection.sendTransaction(signedTransaction);
+
+  const latestBlockHash = await connection.getLatestBlockhash();
+  const confirmationPromise = connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: signature,
+  }).then(() => 'Confirmed!');
+
+  return { signature, confirmation: confirmationPromise };
+};
+
+export const getAccountBalance = async (payer: PublicKey, account: PublicKey) => {
+  const connection = new Connection(RPC_URL, 'finalized');
+
+  try {
+    const ligmaAta = await getAssociatedTokenAddress(account, payer);
+    const accountInfo = await getAccount(connection, ligmaAta);
+    const balance = Number(accountInfo.amount) / 1000000;
+    return balance;
+  } catch (error) {
+    console.error("Account not found or other error:", error);
+    return 0;
+  }
+}
